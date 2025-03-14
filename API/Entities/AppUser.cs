a4f03c8c3ce5108f42b956a7212c4c1faeb707ec
Iakovos Kareglis <JacobK7376@gmail.com>
2025-03-14 22:38:24 +0200

    Authentication, Registering and Logging, Secure Password Keeping, DTOs and JWT Tokens

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,8 +4,12 @@ namespace API.Entities;
 
 public class AppUser
 {
-    // Identfier attribute for users
+    // Identifier attribute for users
     public int Id { get; set; }
 
     public required string UserName { get; set; }
+
+    public required byte[] PasswordHash { get; set; }
+
+    public required byte[] PasswordSalt { get; set; }
 }
